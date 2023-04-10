010c513fc0e894b301c18e31c004e37891f830e7
aabdullah27182845 <abdullahnavid5@gmail.com>
2023-04-10 19:35:39 +0100

    added a navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="logo">
+                Logo
+            </div>
+            <div className="links">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Login</a></li>
+                    <li><a href="/">Help</a></li>
+                </ul>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
