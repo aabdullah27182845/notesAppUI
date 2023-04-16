c5f489f5bd1eb6bcc3d7f7e5ecf346b96e5ffae8
aabdullah27182845 <abdullahnavid5@gmail.com>
2023-04-16 23:52:24 +0100

    adding note functionality

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,14 +4,14 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <div className="logo">
-                <Logo />
+                <a href="/"><Logo /></a>
             </div>
             <div className="links">
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/">Login</a></li>
                     <li><a href="/">Help</a></li>
-                    <li><a href="/">Create</a></li>
+                    <li><a href="/notes">Create</a></li>
                 </ul>
             </div>
         </nav>
--- src/Notes.js
+++ src/Notes.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 const Notes = () => {
+
+    const [elements, setElements] = useState([]);
+
+    const changeElements = () => {
+        const newElements = elements.slice(0, 3);
+        console.log(newElements);
+        setElements(null);
+        setElements(newElements);
+    }
+
+    for (let i = 0; i < 3; i++) {
+        elements.push(
+        <div className="note" key={i}>
+            <h2>Hello</h2>
+        </div>);
+    }
+
     return ( 
         <div className="notes">
             <button>Create New Note</button>
-            <div className="note">
-                <h2>Hello</h2>
+            <div className="note-container">
+                <div className="note">
+                    <h2>Hello</h2>
+                </div>
+                { elements }
             </div>
+            <button onClick={changeElements}>Hello</button>
         </div>
      );
 }
